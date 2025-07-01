cc40acfd28594eff5283737955590b8c32c1b18d
VikashMalav <malavvicky99@gmail.com>
2025-07-01 20:14:44 +0530

    pagination added

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -3,8 +3,12 @@ const JobModel = require("../models/JobModel")
 
 exports.getAllJobs = async (req, res, next) => {
     try {
-        const jobs = await JobModel.find().populate("company","name")
-        res.json({ success: true, message: "All Jobs List", jobs, length: jobs.length })
+        const { page = 1, limit = 5 } = req.query
+        const skip = (page - 1) * limit
+
+        const jobs = await JobModel.find().skip(parseInt(skip)).limit(parseInt(limit)).sort({createdAt:-1}).populate("company", "name")
+        const total =await JobModel.countDocuments()
+        res.json({ success: true, message: "All Jobs List", jobs,page:parseInt(page), totalJobs:total,totalPages:Math.ceil(total/limit) })
     } catch (error) {
         console.log(error)
         next(error)
@@ -18,7 +22,7 @@ exports.getJobById = async (req, res, next) => {
         if (!mongoose.Types.ObjectId.isValid(id)) {
             res.status(400).json({ success: false, message: "Invalid ID " })
         }
-         
+
         const job = await JobModel.findById(id).populate("company")
         if (!job) return res.status(404).json({ success: false, message: "Job Not Found" })
         res.json({ success: true, message: "Job Details", job })
@@ -31,21 +35,21 @@ exports.getJobById = async (req, res, next) => {
 
 exports.searchJobs = async (req, res, next) => {
     try {
-       
-        const {q}=req.query
-        if(!q &&  !q.trim())return res.json({success:false,message:"Search Query is Required"})
-            const trimQuery =q.trim()
-             const searchResult = await JobModel.find({
+
+        const { q } = req.query
+        if (!q && !q.trim()) return res.json({ success: false, message: "Search Query is Required" })
+        const trimQuery = q.trim()
+        const searchResult = await JobModel.find({
             $or: [
                 { title: { $regex: trimQuery, $options: "i" } },
                 { description: { $regex: trimQuery, $options: "i" } },
-                 { skills: { $regex: trimQuery, $options: "i" } },
+                { skills: { $regex: trimQuery, $options: "i" } },
             ]
         }).populate("company")
         console.log(searchResult)
-        if(searchResult.length===0) return res.status(404).json({success:false,message:"Result Not Found...",searchResult})
-        
-        res.json({success:true,message:"Find Query Result ",searchResult,length:searchResult.length})
+        if (searchResult.length === 0) return res.status(404).json({ success: false, message: "Result Not Found...", searchResult })
+
+        res.json({ success: true, message: "Find Query Result ", searchResult, length: searchResult.length })
     } catch (error) {
         console.log(error)
         next(error)
